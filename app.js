4c6b3a09ded8839a2d6cf8d10b1075f3fb98ab8b
louiselynggaard <70694477+louiselynggaard@users.noreply.github.com>
2020-11-29 15:46:42 +0100

    Det er nu muligt at oprette brugere

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const app = express();
 
 //Middleware
+app.use(bodyParser.json()); //gør det muligt at læse 'body' via Express
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/views'));
 
 const userRoute = require('./routes/user.route');
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,15 +2,29 @@ const fs = require('fs');
 
 const User = require('../models/user.model'); //klasse hentes fra denne mappe.
 
+function readUserData() { //sørger for at hver gang serveren startes, så loades alt hvad der er tilgængeligt i "databasen" user.data.json
+    let fileData = fs.readFileSync('./app_data/user.data.json', 'utf8', function (err) {
+        if (err) return console.log(err);
+    });
+       
+    console.log('Loaded: user.data.json');
+    return JSON.parse(fileData);
+};
+
+var userData = readUserData();
+
 exports.user_create = function (req, res) {
-    let obj = new User(
+    let user = new User(
         (++userData.lastUserId).toString(),
         req.body.name,
         req.body.dateOfBirth,
-        req.zipCode,
-        req.email,
-        req.password
+        req.body.zipCode,
+        req.body.email,
+        req.body.password
     );
 
-    console.log('user_create, object:', obj);
+    userData.userList.push(user); //indsæt som statisk data i JSON-fil
+    console.log('user_create, user:', user);
+
+    res.send(user)
 };
